d09c2f960f50849113296da48139b7c26eca740a
destefanis <drdestefanis@gmail.com>
2017-12-14 20:56:57 -0600

    Started the project over for Destiny with new endpoints and front end

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import uglify from 'rollup-plugin-uglify'
-import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+import builtins from 'rollup-plugin-node-builtins';
+import json from 'rollup-plugin-json';
+import globals from 'rollup-plugin-node-globals';
  
-var productionConfig =
+const productionConfig =
 {
    entry:  'main.js',
    dest:   'bundle.js',
    format: 'iife',
    plugins:
    [
-      babel({
-         exclude: 'node_modules/**'
-      }),
       nodeResolve({
-        jsnext: true
+         jsnext: true,
+         main: true,
+         extensions: [ '.js', '.json' ],
+         browser: true,
+         preferBuiltins: false
       }),
       commonjs({
          include: 'node_modules/**',
@@ -24,8 +28,14 @@ var productionConfig =
             './node_modules/react/react.js': 
             [ 'cloneElement', 'createElement', 'PropTypes', 
               'Children', 'Component' ],
+            // './node_modules/the-traveler/build/traveler.js': [ 'Traveler' ],
          }
       }),
+      babel({
+         exclude: 'node_modules/**'
+      }),
+      globals(),
+      builtins(),
       replace({
          'process.env.NODE_ENV': JSON.stringify( 'production' )
       }),
@@ -38,35 +48,45 @@ var productionConfig =
             comments: false
          },
          sourceMap: false
-      })
+      }),
+      json()
    ]
 }
 
-var developmentConfig =
+const developmentConfig =
 {
    entry:  'src/js/main.js',
    dest:   'dist/bundle.js',
    format: 'iife',
    plugins:
    [
-      babel({
-         exclude: 'node_modules/**'
-      }),
       nodeResolve({
-        jsnext: true
+         jsnext: true,
+         main: true,
+         extensions: [ '.js', '.json' ],
+         browser: true,
+         preferBuiltins: false
       }),
       commonjs({
          include: 'node_modules/**',
          namedExports:
          {
-            './node_modules/react/react.js':      
-            [ 'cloneElement', 'createElement', 'PropTypes', 
+            './node_modules/react/react.js': [ 'cloneElement', 'createElement', 'PropTypes', 
               'Children', 'Component' ],
+            // 'node_modules/the-traveler/build/traveler.js': [ 'Traveler', 'traveler' ],
+            // 'node_modules/punycode/punycode.js': ['toASCII'],
+            // 'node_modules/buffer-es6/index.js': ['def' + 'ault'],
          }
       }),
+      babel({
+         exclude: 'node_modules/**'
+      }),
+      globals(),
+      builtins(),
       replace({
          'process.env.NODE_ENV': JSON.stringify( 'development' )
-      })
+      }),
+      json()
    ]
 }
 // Edit this line for dev configs (either Dev or Production)
--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -1,13 +1,112 @@
 // Import React.
 import React from 'react'
 
-export class TestComponent extends React.Component {
+const api_key = "b8f2f9674ea24761bfe8f0a49a84d3a3";
+const host = 'https://www.bungie.net/Platform/Destiny2/';
+const requestHeaders = {
+      method: 'GET',
+      mode: 'cors',
+      headers: new Headers({
+        "X-API-Key": api_key
+      })
+    };
+
+class PlatformSelect extends React.Component {
+  render() {
+    return (
+      <div>
+        <label className="form-label form-label--transparent">
+          Platform
+        </label>
+        <div>
+          <input type="radio" id="platform-1" name="platform" value="4" />
+          <label htmlFor="platform-1">PC</label>
+          <input type="radio" id="platform-2" name="platform" value="1" />
+          <label htmlFor="platform-2">PS4</label>
+          <input type="radio" id="platform-3"  name="platform" value="2" />
+          <label htmlFor="platform-3">Xbox</label>
+        </div>
+      </div>
+    );
+  }
+}
+
+class PlayerInfoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submittedName: '',
+      submittedPlatform: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({submittedName: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let playerName = encodeURIComponent(this.state.submittedName);
+    let endpoint = host + 'SearchDestinyPlayer/4/' + playerName + '/';
+    let request = new Request(endpoint, requestHeaders);
+
+    // Fetch the MembershipID for the user.
+    // The membership ID is used to access the bungie.net profile and game activity.
+    fetch(request)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+
+        let membershipId = data.Response[0].membershipId;
+        // Update the endpoint to request the users profile.
+        endpoint = host + '4/Profile/' + membershipId + '/?components=100';
+        // Create a new request with the update endpoint.
+        let userRequest = new Request(endpoint, requestHeaders);
+
+        return fetch(userRequest)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(function(error) { 
+        console.log('Requestfailed', error) 
+      });
+  }
+
+  render() {
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        <PlatformSelect />
+        <input type="text" placeholder="YourName#1377" value={this.state.playerName} onChange={this.handleChange} />
+        <div>
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export class DefaultComponent extends React.Component {
+  constructor() {
+    super();
+  }
+
   render () {
     return (
-      <article>
-        <h1>Hi from TestComponent.</h1>
-        <em>Now let's play with React!</em>
-      </article>
+      <div>
+        <nav className="nav">
+          <h1 className="h1">Destiny Medals</h1>
+        </nav>
+        <section className="main">
+          <PlayerInfoForm />
+        </section>
+      </div>
     )
   }
 }
+
